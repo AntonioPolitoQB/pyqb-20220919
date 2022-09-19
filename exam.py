80ce943fe98b40e0ad7e6eac1b227d4a63a607c1
Mattia Monga <mattia.monga@unimi.it>
2022-09-19 09:51:34 +0200

    Correct typo

--- exam.py
+++ exam.py
@@ -41,7 +41,7 @@ pass
 
 # ### Exercise 2 (max 5 points)
 #
-# Make a figure with two plots: a scatterplot of the `x` and `y` values; each point should be colored according its `subarea`. Use a proper title and a legend (Hint: https://matplotlib.org/stable/gallery/lines_bars_and_markers/scatter_with_legend.html).
+# Make a figure with a scatterplot of the `x` and `y` values; each point should be colored according its `subarea`. Use a proper title and a legend (Hint: https://matplotlib.org/stable/gallery/lines_bars_and_markers/scatter_with_legend.html).
 #
 
 pass
